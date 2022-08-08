ec67c5786e08bac6488c14081c8d4aff31ed3cb7
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-08-08 11:35:06 -0500

    finshed exercise

---
+++ intro.js
@@ -0,0 +1,26 @@
+//Exercise 1 and 2
+const firstName = 'Alex';
+let lastName = 'Coleman';
+let age = 25;
+
+//Exercise 3
+let language = 'Javascript';
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let isStock = false;
+let selectSize = 'M';
+
+//Exercise 5
+let title = 'Name of the wind';
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
